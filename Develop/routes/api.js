6ee757d6722fab10a281a92a0d79fa4ecc9ed29f
valbona1992 <valbona12@gmail.com>
2021-10-10 19:21:10 -0500

    add API route to get the dashboard range of total duration

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -1,3 +1,22 @@
 const router = require('express').Router();
-const Workout = require("../models");
+const Workout = require("../models/workout");
+
+
+// Route to get the dashboard for seven days 
+router.get("/api/workouts/range", (req, res) => {
+    Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: { $sum: $exercises.duration }
+            }
+        }
+    ])
+        .sort({ day: -1}).limit(7)
+        .then(workoutDB => {
+            res.json(workoutDB);
+        }).catch(err => {
+            console.log(err);
+            res.sendStatus(500);
+        })
+});
 
