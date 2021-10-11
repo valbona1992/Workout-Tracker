17e9aebe5353c4e49d2b9a850fd409f840c82b67
valbona1992 <valbona12@gmail.com>
2021-10-10 19:03:34 -0500

    add workout model for tracking each exercise

---
+++ Develop/models/workout.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    day: {
+        type: Date, 
+        default: Date.now,
+    },
+
+    exercises: [{
+        name: {
+            type: String, 
+            trim: true,
+        },
+        type: {
+            type: String, 
+            trim: true,
+        },
+        weight: {
+            type: Number
+
+        },
+        sets: {
+            type: Number
+        },
+        reps: {
+            type: Number
+        },
+        duration: {
+            type: Number
+        },
+        distance: {
+            type: Number
+        }
+    }],
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+module.exports = Workout;
